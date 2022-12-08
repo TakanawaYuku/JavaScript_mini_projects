4df6c71f688854875827cc4583523c8e6733cfb9
TakanawaYuku <yukutakanawa@gmail.com>
2022-12-08 19:53:48 +0300

    add files project random color

---
+++ random_colors/js/script.js
@@ -0,0 +1,28 @@
+const cols = document.querySelectorAll('.col');
+
+
+function generateRandomColor() {
+    //RGB
+    //#FF0000
+    //#00FF00
+    //#0000FF
+
+    const hexCodes = '01234567879ABCDEF';
+
+    let color = '';
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+
+    return '#' + color
+}
+
+
+
+function setRandomColors() {
+    cols.forEach(col => {
+        console.log(col)
+    })
+}
+
+setRandomColors()
