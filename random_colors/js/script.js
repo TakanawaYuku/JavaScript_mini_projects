47d3dbb29a1dd469db204b57699bc170b62ef738
TakanawaYuku <yukutakanawa@gmail.com>
2022-12-09 19:18:38 +0300

    func setTextColor

--- random_colors/js/script.js
+++ random_colors/js/script.js
@@ -11,23 +11,32 @@ function generateRandomColor() {
 
     let color = '';
     for (let i = 0; i < 6; i++) {
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+    };
 
     return '#' + color
-}
+};
 
 
 
 function setRandomColors() {
     cols.forEach(col => {
-        const text_title = col.querySelector('h2')
-        const color = chroma.random()
+        const text = col.querySelector('h2');
+        const color = chroma.random();
 
 
-        text_title.textContent = color
-        col.style.background = color
-    })
-}
+        text.textContent = color;
+        col.style.background = color;
 
-setRandomColors()
+        setTextColor(text,color);
+    });
+};
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance();
+
+    text.style.color = luminance > 0.5 ? 'black' : 'white';
+};
+
+
+setRandomColors();
