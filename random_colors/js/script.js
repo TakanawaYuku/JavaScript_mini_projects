af34da5442beb77098cb2e6ac19d7e48391f1628
TakanawaYuku <yukutakanawa@gmail.com>
2022-12-12 19:26:20 +0300

    additions to functions changing them

--- random_colors/js/script.js
+++ random_colors/js/script.js
@@ -44,13 +44,14 @@ function copyToClickBoard(text) {
     return navigator.clipboard.writeText(text)
 }
 
-function setRandomColors() {
-    const colors = []
-    cols.forEach(col => {
+function setRandomColors(isInitial) {
+    const colors = isInitial ? getColorsFromHash() : [];
+
+    cols.forEach((col, index) => {
         const isLocked = col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector('h2');
         const button = col.querySelector('button');
-        const color = chroma.random();
+
 
 
         if (isLocked) {
@@ -58,7 +59,17 @@ function setRandomColors() {
             return
         }
 
-        colors.push(color);
+        const color = isInitial
+        ? colors[index]
+            ? colors[index]
+            : chroma.random()
+        : chroma.random()
+
+        if (!isInitial) {
+            colors.push(color);
+        };
+
+
 
         text.textContent = color;
         col.style.background = color;
@@ -88,7 +99,7 @@ function updateColorsHash(colors=[]) {
 
 function getColorsFromHash() {
     if (document.location.hash.length > 1) {
-        document.location.hash
+        return document.location.hash
         .substring(1)
         .split('-')
         .map(color => '#' + color)
@@ -97,4 +108,4 @@ function getColorsFromHash() {
 };
 
 
-setRandomColors();
+setRandomColors(true);
